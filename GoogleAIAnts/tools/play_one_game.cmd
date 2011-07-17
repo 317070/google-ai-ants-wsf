6efba33538ffea170778710b68b68cde48794f2b
erstaateenknolraapinmijntuin@gmail.com <erstaateenknolraapinmijntuin@gmail.com@8ebac2a7-6433-c199-e73d-8c2311747907>
2011-07-17 12:36:47 +0000

    Log wordt automatisch op de juiste plaats aangemaakt door de compiler.

--- GoogleAIAnts/tools/play_one_game.cmd
+++ GoogleAIAnts/tools/play_one_game.cmd
@@ -1,9 +1,10 @@
 @echo off
-playgame.py --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --food=random --turns 1000 --map_file maps\test_maps\testmap.map %* "java -jar ../dist/GoogleAIAnts.jar" "python sample_bots\python\HunterBot.py"
-
+::test
+:: playgame.py --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --food=random --turns 100 --map_file maps\test_maps\testmap.map %* "java -jar ../dist/GoogleAIAnts.jar" "python sample_bots\python\HunterBot.py"
+::game
+playgame.py --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 1000 --map_file maps\symmetric_maps\symmetric_10.map %* "java -jar ../dist/GoogleAIAnts.jar" "python sample_bots\python\LeftyBot.py" "python sample_bots\python\HunterBot.py" "python sample_bots\python\GreedyBot.py"
 
 ::comments
-:: playgame.py --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 1000 --map_file maps\symmetric_maps\symmetric_10.map %* "java -jar ../dist/GoogleAIAnts.jar" "python sample_bots\python\LeftyBot.py" "python sample_bots\python\HunterBot.py" "python sample_bots\python\LeftyBot.py"
 
 ::Options:
 ::  -h, --help            show this help message and exit
